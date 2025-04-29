a9de10f33da7379ad8c3b1a2d2457785c7cde98c
Kasyanov-Igor <igor.kasyanov00@mail.ru>
2025-04-29 21:19:15 +0300

    Added Admin model. Closed #14

--- gym-project-business-logic/Model/Entities/Admin.cs
+++ gym-project-business-logic/Model/Entities/Admin.cs
@@ -1,7 +1,25 @@
-﻿namespace gym_project_business_logic.Model
+﻿using gym_project_business_logic.Model.Enum;
+using System;
+
+namespace gym_project_business_logic.Model
 {
     public class Admin
     {
         public int Id { get; set; }
+        public string FullName { get; set; } = null!;
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+
+        public string Gender { get; set; } = null!;
+
+        public AdminStatus status { get; set; }
+
+        public string Login { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
     }
 }
--- gym-project-business-logic/Services/AdminService.cs
+++ gym-project-business-logic/Services/AdminService.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace gym_project_business_logic.Services
 {
-    internal class AdminService
+    public class AdminService
     {
+
     }
 }
