df24b3e288d70b35fb1051148bf816765bf57766
Igor <161635179+Kasyanov-Igor@users.noreply.github.com>
2025-05-04 12:11:15 +0300

    Added AutoMapper for Coach. Closed #18

---
+++ gym-project-business-logic/Services/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using gym_project_business_logic.Model;
+using gym_project_business_logic.Model.Domains;
+
+namespace gym_project_business_logic.Services
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<DTOCoach, Coach>();
+        }
+    }
+}
--- gym-project/Controllers/CoachController.cs
+++ gym-project/Controllers/CoachController.cs
@@ -1,48 +1,61 @@
+using AutoMapper;
 using gym_project_business_logic.Model;
+using gym_project_business_logic.Model.Domains;
 using gym_project_business_logic.Services;
 using gym_project_business_logic.Services.Interface;
 using Microsoft.AspNetCore.Mvc;
 
 namespace gym_project.Controllers
 {
-	[ApiController]
-	[Route("[controller]")]
-	public class CoachController : ControllerBase
-	{
-		private ICoachService _coachService;
+    [ApiController]
+    [Route("[controller]")]
+    public class CoachController : ControllerBase
+    {
+        private ICoachService _coachService;
 
-		private ADatabaseConnection _connection;
+        private ADatabaseConnection _connection;
 
-		public CoachController()
-		{
-			this._connection = new SqliteConnection();
-			this._coachService = new CoachService(this._connection);
-		}
+        private IMapper _mapper;
 
-		[HttpPost("Register")]
-		public void RegisterController(Coach model)
-		{
-			if (ModelState.IsValid)
-			{
-				Coach coach = new Coach
-				{
-					FullName = model.FullName,
-					DateOfBirth = model.DateOfBirth,
-					Email = model.Email,
-					PhoneNumber = model.PhoneNumber,
-					Gender = model.Gender,
-					Specialization = model.Specialization,
-					Status = model.Status,
-					Login = model.Login,
-					Password = model.Password,
-					WorkingTime = model.WorkingTime
-				};
+        public CoachController(IMapper mapper)
+        {
+            this._connection = new SqliteConnection();
+            this._coachService = new CoachService(this._connection);
+            this._mapper = mapper;
 
-				if (coach.Password != null && coach.Password != null && coach.FullName != null)
-				{
-					this._coachService.Registration(coach);
-				}
-			}
-		}
-	}
+        }
+
+        [HttpPost("register")]
+        public void RegisterController([FromBody] DTOCoach modelDTO)
+        {
+            Coach coach = this._mapper.Map<Coach>(modelDTO);
+
+            string salt = PasswordHelper.GenerateSalt();
+            string hashedPassword = PasswordHelper.HashPassword(coach.Password, salt);
+
+
+            //if (ModelState.IsValid)
+            //{
+            //	Coach coach = new Coach
+            //	{
+            //		FullName = model.FullName,
+            //		DateOfBirth = model.DateOfBirth,
+            //		Email = model.Email,
+            //		PhoneNumber = model.PhoneNumber,
+            //		Gender = model.Gender,
+            //		Specialization = model.Specialization,
+            //		Status = model.Status,
+            //		Login = model.Login,
+            //		Password = model.Password,
+            //		WorkingTime = model.WorkingTime
+            //	};
+
+            //	if (coach.Password != null && coach.Password != null && coach.FullName != null)
+            //	{
+            //		this._coachService.Registration(coach);
+            //	}
+        }
+    }
+
 }
+
