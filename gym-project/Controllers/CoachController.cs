9e3d5fdaa8a2581399df4ea1127b2807df5668d5
Igor <161635179+Kasyanov-Igor@users.noreply.github.com>
2025-05-04 12:15:29 +0300

    Added AutoMapper for Coach. Closed #18

--- gym-project/Controllers/CoachController.cs
+++ gym-project/Controllers/CoachController.cs
@@ -7,55 +7,55 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace gym_project.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class CoachController : ControllerBase
-    {
-        private ICoachService _coachService;
+	[ApiController]
+	[Route("[controller]")]
+	public class CoachController : ControllerBase
+	{
+		private ICoachService _coachService;
 
-        private ADatabaseConnection _connection;
+		private ADatabaseConnection _connection;
 
-        private IMapper _mapper;
+		private IMapper _mapper;
 
-        public CoachController(IMapper mapper)
-        {
-            this._connection = new SqliteConnection();
-            this._coachService = new CoachService(this._connection);
-            this._mapper = mapper;
+		public CoachController(IMapper mapper)
+		{
+			this._connection = new SqliteConnection();
+			this._coachService = new CoachService(this._connection);
+			this._mapper = mapper;
 
-        }
+		}
 
-        [HttpPost("register")]
-        public void RegisterController([FromBody] DTOCoach modelDTO)
-        {
-            Coach coach = this._mapper.Map<Coach>(modelDTO);
+		[HttpPost("register")]
+		public void RegisterController([FromBody] DTOCoach modelDTO)
+		{
+			Coach coach = this._mapper.Map<Coach>(modelDTO);
 
-            string salt = PasswordHelper.GenerateSalt();
-            string hashedPassword = PasswordHelper.HashPassword(coach.Password, salt);
+			//string salt = PasswordHelper.GenerateSalt();
+			//string hashedPassword = PasswordHelper.HashPassword(coach.Password, salt);
 
 
-            //if (ModelState.IsValid)
-            //{
-            //	Coach coach = new Coach
-            //	{
-            //		FullName = model.FullName,
-            //		DateOfBirth = model.DateOfBirth,
-            //		Email = model.Email,
-            //		PhoneNumber = model.PhoneNumber,
-            //		Gender = model.Gender,
-            //		Specialization = model.Specialization,
-            //		Status = model.Status,
-            //		Login = model.Login,
-            //		Password = model.Password,
-            //		WorkingTime = model.WorkingTime
-            //	};
+			//if (ModelState.IsValid)
+			//{
+			//	Coach coach = new Coach
+			//	{
+			//		FullName = model.FullName,
+			//		DateOfBirth = model.DateOfBirth,
+			//		Email = model.Email,
+			//		PhoneNumber = model.PhoneNumber,
+			//		Gender = model.Gender,
+			//		Specialization = model.Specialization,
+			//		Status = model.Status,
+			//		Login = model.Login,
+			//		Password = model.Password,
+			//		WorkingTime = model.WorkingTime
+			//	};
 
-            //	if (coach.Password != null && coach.Password != null && coach.FullName != null)
-            //	{
-            //		this._coachService.Registration(coach);
-            //	}
-        }
-    }
+			//	if (coach.Password != null && coach.Password != null && coach.FullName != null)
+			//	{
+			//		this._coachService.Registration(coach);
+			//	}
+		}
+	}
 
 }
 
