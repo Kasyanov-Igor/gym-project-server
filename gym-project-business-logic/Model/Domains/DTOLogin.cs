fe3bd1573572063fe03d8960b38150b3eaa5aa4c
Igor <161635179+Kasyanov-Igor@users.noreply.github.com>
2025-06-11 20:28:11 +0300

    Moved the business logic to a separate project. Closed #40 (#59)
    * Moved the business logic to a separate project. Closed #40
    
    * Add Update dotnet.yml

--- gym-project-business-logic/Model/Domains/DTOLogin.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace gym_project_business_logic.Model.Domains
-{
-	public class DTOLogin
-	{
-		public string Login { get; set; } = null!;
-
-		public string Password { get; set; } = null!;
-	}
-}
--- gym-project-business-logic/Model/Enum/TrainerSpecializationEnum.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace gym_project_business_logic.Model.Enum
-{
-    public enum TrainerSpecializationEnum
-    {
-        PersonalTraining = 0,
-        Dancing = 1,
-        Yoga = 2
-    }
-}
