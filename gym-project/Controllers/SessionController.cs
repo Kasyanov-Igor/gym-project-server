7db03f97d63cf60406aac6f1f0a7504c0f503cfd
Kasyanov-Igor <igor.kasyanov00@mail.ru>
2025-06-12 21:33:00 +0300

    Add Asp Net Core Session. Closed #62

---
+++ gym-project/Controllers/SessionController.cs
@@ -0,0 +1,34 @@
+﻿using gym_project_business_logic.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gym_project.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class SessionController : ControllerBase
+	{
+		private ITokenService _tokenService;
+
+		SessionController(ITokenService tokenService)
+		{
+			this._tokenService = tokenService;
+		}
+
+		[HttpGet("set")]
+		public IActionResult SetSession(string token)
+		{
+			HttpContext.Session.SetString("token", token);
+			return Ok("Session value set");
+		}
+
+		[HttpGet("get")]
+		public IActionResult GetSession()
+		{
+			var userName = HttpContext.Session.GetString("UserName");
+			if (string.IsNullOrEmpty(userName))
+				return NotFound("Session value not found");
+
+			return Ok($"UserName from session: {userName}");
+		}
+	}
+}
---
+++ gym-project/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace gym_project
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            // Добавляем сервисы для сессий
+            services.AddDistributedMemoryCache(); // Хранилище сессий в памяти
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30); // Время жизни сессии
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Другие middleware
+            app.UseRouting();
+
+            app.UseSession(); // Добавляем поддержку сессий
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
